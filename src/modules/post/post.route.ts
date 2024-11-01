a7743d9ccaab46bda3eb2ff5d394c1547a9e7804
JfJubaer <skk5727@gmail.com>
2024-11-02 04:23:52 +0600

    searchterm

---
+++ src/modules/post/post.route.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import { PostController } from './post.controller';
+
+const router = express.Router();
+
+router.get("/", PostController.getAllPostController)
+router.get("/:id", PostController.getSinglePostController)
+router.post("/create-post", PostController.createPostController)
+router.patch('/:id', PostController.updatePost)
+router.delete('/:id', PostController.deletePost)
+router.get('/learn-query', PostController.learnAggregateAndGrouping)
+
+export const PostRoutes = router;
